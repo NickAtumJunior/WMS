4667f5f561457bf0b34bf82f1130ce1cc28362b7
NickAtumJunior <nithishthirucelvam007@gmail.com>
2022-06-15 18:05:56 +0530

    new commit today

---
+++ src/components/LoadingBar.js
@@ -0,0 +1,12 @@
+import { LinearProgress } from '@mui/material'
+import React from 'react'
+
+function LoadingBar() {
+  return (
+    <div>
+        <LinearProgress/>
+    </div>
+  )
+}
+
+export default LoadingBar
---
+++ src/components/SidenavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidenavBar() {
+  return (
+    <div>SidenavBar</div>
+  )
+}
+
+export default SidenavBar
